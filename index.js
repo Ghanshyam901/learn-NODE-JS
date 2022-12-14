21075ae7fa34d696fe57b466588ba4dc5f14b3df
Ghanshyam ram <Ghanshyam901@users.noreply.github.com>
2022-12-15 01:20:42 +0530

    file system

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-// console.log is global module .
-console.log("heelo node js")
-// fs is a non global module..
-fs.writeFileSync("hello.txt"," hello node");
+// const fs = require('fs');
+
+// // fs.writeFileSync("hello.txt"," hello node");
+// // fs.writeFileSync("nd.js"," hello node");
+
+// console.log(__dirname); // for directry find
+// console.log(__filename); // fro dinf file name 
