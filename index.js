63fd43ac9151a70283f2faee0da98eebfff7a248
Ghanshyam ram <Ghanshyam901@users.noreply.github.com>
2022-12-15 01:04:40 +0530

    basic

---
+++ index.js
@@ -0,0 +1,26 @@
+const app = require('./app')
+console.log(app);
+console.log(app.z()) 
+
+
+////////////////////////////////////////////////////////
+
+// console.log("hello")
+// var a = 20;
+// var b = 30;
+// console.log(a+b);
+
+// /// fundamental of node js
+
+// if(a === 20){
+//     console.log("true")
+// }
+// for(i =0;i<=10; i++){
+//     console.log(i);
+// }
+ 
+//===========================================
+
+// var x = '20'
+// const arr =[2,3,4,5,6,7,8,9];
+// console.log(arr[0]);
