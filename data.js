dc6549f4418430ec9b46f3bd3e4280e784e6661c
Ghanshyam ram <Ghanshyam901@users.noreply.github.com>
2022-12-15 02:21:30 +0530

    simple api

---
+++ data.js
@@ -0,0 +1,9 @@
+
+const data =[
+    {name:'ghanshyam kumar',emai :'ghanshyma901@gmail.com',contact :990022},
+    {name:'shyam kumar',emai :'ghanshyma901@gmail.com'},
+    {name:'shyam kumar',emai :'ghanshyma901@gmail.com'},
+    {name:'sshyam kumar',emai :'ghanshyma901@gmail.com'},
+    {name:'manshyam kumar',emai :'ghanshyma901@gmail.com'}
+]
+module.exports=data;
--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-const color = require('colors');
+const http = require('http');
+const data = require('./data');
+http.createServer((request,response)=>{
+    response.writeHead(200,{'content-Type' :'application/json'});
+    response.write(JSON.stringify(data));
+    response.end();
+}).listen(5000);
 
-console.log("hello".red);
-console.log("hello".green);
+console.log("h");
