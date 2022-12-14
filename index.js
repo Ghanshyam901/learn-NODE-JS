29406c0945dfe57e487c47ecf450909254d710f9
Ghanshyam ram <Ghanshyam901@users.noreply.github.com>
2022-12-15 01:16:04 +0530

    globalr

--- index.js
+++ index.js
@@ -1,44 +1,4 @@
-const app = require('./app')
-// console.log(app);
-// console.log(app.z()) 
-
-
-////////////////////////////////////////////////////////
-
-// console.log("hello")
-// var a = 20;
-// var b = 30;
-// console.log(a+b);
-
-// /// fundamental of node js
-
-// if(a === 20){
-//     console.log("true")
-// }
-// for(i =0;i<=10; i++){
-//     console.log(i);
-// }
- 
-//===========================================
-
-// var x = '20'
-// const arr =[2,3,4,5,6,7,8,9];
-// console.log(arr[0]);
-
-// ----------------------------------- filter
-/// filter is find the specific value it always operate on arrays..
-const arr =[2,3,4,5,6,4,8,9];
-
-// arr.filter((item)=>{
-//     console.log(item);
-// })
-
- let res = arr.filter((item)=>{
-    return item > 4
-})
-console.log(res)
-
-let ans = arr.filter((item)=>{
-    return item == 4
-})
-console.log(ans);
+// console.log is global module .
+console.log("heelo node js")
+// fs is a non global module..
+fs.writeFileSync("hello.txt"," hello node");
