3723c7e1e6f2e0dd7a985a89681eba9fd13e54fc
Ghanshyam ram <Ghanshyam901@users.noreply.github.com>
2022-12-15 15:11:20 +0530

    file creation in loop in directory and reading file

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-const http = require('http');
-const data = require('./data');
-http.createServer((request,response)=>{
-    response.writeHead(200,{'content-Type' :'application/json'});
-    response.write(JSON.stringify(data));
-    response.end();
-}).listen(5000);
+const fs = require('fs') /// file system module;
+const path = require('path');
 
-console.log("h");
+
+const dirPath = path.join(__dirname,'files');
+console.log(dirPath);
+
+// for(i=1; i<=5; i++){
+//     fs.writeFileSync(dirPath+"/hellooo"+i+".js","simp file");
+//     // fs.writeFileSync(dirPath+`/hello${i}.txt`,"simple file creation ");
+// }
+
+// fs.readdir(dirPath,(err, filess)=>{
+//     console.log(filess);
+// });
+
+fs.readdir(dirPath,(err, filess)=>{
+    filess.forEach((item)=>{
+        console.log( "file name is -> "+item);
+    })
+});
