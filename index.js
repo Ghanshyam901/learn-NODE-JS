c6a002330bb5401d6b483812209e5224b19eaa24
Ghanshyam ram <Ghanshyam901@users.noreply.github.com>
2022-12-15 15:38:15 +0530

    crud normal file operation

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
 const fs = require('fs') /// file system module;
 const path = require('path');
+const dirpath = path.join(__dirname,'crud');
+const filePath = `${dirpath}/orange.txt`;
 
+// fs.writeFileSync(filePath,"this is simple crud file ") // crate file
 
-const dirPath = path.join(__dirname,'files');
-console.log(dirPath);
-
-// for(i=1; i<=5; i++){
-//     fs.writeFileSync(dirPath+"/hellooo"+i+".js","simp file");
-//     // fs.writeFileSync(dirPath+`/hello${i}.txt`,"simple file creation ");
-// }
-
-// fs.readdir(dirPath,(err, filess)=>{
-//     console.log(filess);
+// fs.readFile(filePath,(err , item)=>{     //read
+//     console.log(item);
 // });
 
-fs.readdir(dirPath,(err, filess)=>{
-    filess.forEach((item)=>{
-        console.log( "file name is -> "+item);
-    })
-});
+// fs.readFile(filePath,'utf8',(err , item)=>{   // read
+//     console.log(item);
+// });
+
+// fs.appendFile(filePath," hello i am simple file",(err)=>{   // update
+//     if(!err){
+//         console.log("upated successfullt");
+//     }
+// })
+
+// fs.rename(filePath,`${dirpath}/apple.txt`,(err)=>{
+//         if(!err){
+//             console.log("file name is updated");
+//         }
+// })
+
+// fs.unlinkSync(`${dirpath}/apple.txt`);  /// delete file
+
+
