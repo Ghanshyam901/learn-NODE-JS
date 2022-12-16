b656a2b69e1ed670e91ccaef09d2674f08745ca1
Ghanshyam ram <Ghanshyam901@users.noreply.github.com>
2022-12-17 00:56:30 +0530

    html page api

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 // https://www.youtube.com/watch?v=VrQgmNY96wo&t=6122s
 
 const express = require("express");
+const path = require ('path');
+
 const app = express(); // executeable
+const publicPath = path.join(__dirname,'public');
 
-app.get("/", (req, res) => {
-  res.send("hello express js...... home page");
-});
+app.use(express.static(publicPath));
 
-app.get("/about", (req, res) => {
-  res.send("hello express js...... about page");
-});
-
-app.get("/carrier", (req, res) => {
-    res.send("hello express js...... carrier page");
-  });
 
 app.listen(5000);
