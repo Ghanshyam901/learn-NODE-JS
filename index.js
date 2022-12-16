ae00cf24bd79681c42f846d36270a6aa1605f26d
Ghanshyam ram <Ghanshyam901@users.noreply.github.com>
2022-12-17 01:15:50 +0530

    ejs page

--- index.js
+++ index.js
@@ -6,10 +6,21 @@ const path = require ('path');
 const app = express(); // executeable
 const publicPath = path.join(__dirname,'public');
 
-// app.use(express.static(publicPath));
+app.set('view engine','ejs');
+
 app.get('/',(req,res)=>{
     res.sendFile(`${publicPath}/home.html`)
 })
+
+app.get('/profile',(req,res)=>{
+    const user ={
+        name:'sk',
+        email:'sk@gmail.com',
+        contact:923444433
+    }
+    res.render('profile',{user})
+})
+
 app.get('/about',(req,res)=>{
     res.sendFile(`${publicPath}/about.html`)
 })
