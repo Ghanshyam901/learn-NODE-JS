1eff967112bfd9cfcfd6ddde5d9df4d8bfc6f8c4
Ghanshyam ram <Ghanshyam901@users.noreply.github.com>
2022-12-17 01:06:55 +0530

    remove html extensions

--- index.js
+++ index.js
@@ -6,7 +6,19 @@ const path = require ('path');
 const app = express(); // executeable
 const publicPath = path.join(__dirname,'public');
 
-app.use(express.static(publicPath));
+// app.use(express.static(publicPath));
+app.get('/',(req,res)=>{
+    res.sendFile(`${publicPath}/home.html`)
+})
+app.get('/about',(req,res)=>{
+    res.sendFile(`${publicPath}/about.html`)
+})
+
+
+
+app.get('*',(req,res)=>{   // 404 page 
+    res.sendFile(`${publicPath}/pnf.html`)
+})
 
 
 app.listen(5000);
